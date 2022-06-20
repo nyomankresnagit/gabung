696eeba81d2482e03866702983a70cd7da202f30
nyomankresnagit <nyomankresnagithub@gmail.com>
2022-06-20 14:07:05 +0700

    testgabungan

--- config.py
+++ config.py
@@ -1,16 +1,60 @@
 import os
+from dotenv import load_dotenv
+from os.path import join, dirname, realpath
+
+load_dotenv()
+BASEDIR = os.path.abspath(os.path.dirname(realpath(__file__)))
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    host = os.environ.get("DB_HOST")
-    username = os.environ.get("DB_USERNAME")
-    password = os.environ.get("DB_PASSWORD")
-    database = os.environ.get("DATABASE")
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+username+':'+password+'@'+host+'/'+database
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DATABASE = os.getenv('DATABASE')
+    CSVBUKU = os.getenv('CSVBUKU')
+    CSVMEMBER = os.getenv('CSVMEMBER')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_NAME = os.getenv('DB_NAME')
+    FOLDER_DATABASE = os.path.join(BASEDIR, DATABASE)
+    FOLDER_CSVBUKU = os.path.join(BASEDIR, CSVBUKU)
+    FOLDER_CSVMEMBER = os.path.join(BASEDIR, CSVMEMBER)
+    # DATABASE_FILE = "sqlite:///{}".format(os.path.join(FOLDER_DATABASE, 'databaseperpustakaan.db'))
+    # DATABASE_FILE = 'mysql+pymysql://'+DB_USERNAME+':'+DB_PASSWORD+'@'+DB_HOST+'/'+DB_NAME
+    DATABASE_FILE = 'mysql://'+DB_USERNAME+':'+DB_PASSWORD+'@'+DB_HOST+'/'+DB_NAME
+    SQLALCHEMY_DATABASE_URI = DATABASE_FILE
+    CEK_DATABASE = os.path.exists(FOLDER_DATABASE)
+    if not CEK_DATABASE:
+        os.makedirs(FOLDER_DATABASE)
+        print("Folder Database Telah Dibuat.")
+        CEK_CSVBUKU = os.path.exists(FOLDER_CSVBUKU)
+        if not CEK_CSVBUKU:
+            os.makedirs(FOLDER_CSVBUKU)
+            print("Folder Upload CSV Buku Telah Dibuat.")
+            CEK_CSVMEMBER = os.path.exists(FOLDER_CSVMEMBER)
+            if not CEK_CSVMEMBER:
+                os.makedirs(FOLDER_CSVMEMBER)
+                print("Folder Upload CSV Member Telah Dibuat.")
+        else:
+            CEK_CSVMEMBER = os.path.exists(FOLDER_CSVMEMBER)
+            if not CEK_CSVMEMBER:
+                os.makedirs(FOLDER_CSVMEMBER)
+                print("Folder Upload CSV Member Telah Dibuat.")
+    else:
+        CEK_CSVBUKU = os.path.exists(FOLDER_CSVBUKU)
+        if not CEK_CSVBUKU:
+            os.makedirs(FOLDER_CSVBUKU)
+            print("Folder Upload CSV Buku Telah Dibuat.")
+            CEK_CSVMEMBER = os.path.exists(FOLDER_CSVMEMBER)
+            if not CEK_CSVMEMBER:
+                os.makedirs(FOLDER_CSVMEMBER)
+                print("Folder Upload CSV Member Telah Dibuat.")
+        else:
+            CEK_CSVMEMBER = os.path.exists(FOLDER_CSVMEMBER)
+            if not CEK_CSVMEMBER:
+                os.makedirs(FOLDER_CSVMEMBER)
+                print("Folder Upload CSV Member Telah Dibuat.")
 
 
 class ProductionConfig(Config):
@@ -23,12 +67,12 @@ class StagingConfig(Config):
 
 
 class DevelopmentConfig(Config):
-    ENV = "development"
+    ENV = os.getenv('FLASK_ENV')
     DEVELOPMENT = True
-    SECRET_KEY = "secret_for_test_environment"
-    OAUTHLIB_INSECURE_TRANSPORT = True
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    OAUTHLIB_INSECURE_TRANSPORT = os.getenv('OAUTHLIB_INSECURE_TRANSPORT')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    DEBUG = True
+    DEBUG = os.getenv('DEBUG')
 
 
 class TestingConfig(Config):
